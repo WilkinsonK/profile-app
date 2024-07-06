40f7b9041b8f97f16973e04d2b7a27631f9f1804
Keenan W. Wilkinson <keenanwilkinson@outlook.com>
2024-07-06 12:48:58 -0500

    remove unnecessary storage operations

--- client/src/common/view.ts
+++ client/src/common/view.ts
@@ -1,5 +1,3 @@
-const LS_CURRENT_VIEW_KEY: string = "vlb.currentView"
-
 /**
  * Returns the current view from our frontend
  * application. By default, this function will
@@ -7,11 +5,7 @@ const LS_CURRENT_VIEW_KEY: string = "vlb.currentView"
  * was previously set.
  */
 export function getCurrentView(): string {
-    let view = localStorage.getItem(LS_CURRENT_VIEW_KEY);
-    if (view == null) {
-        return window.location.pathname;
-    }
-    return view;
+    return window.location.pathname;
 }
 
 /**
@@ -20,11 +14,4 @@ export function getCurrentView(): string {
  */
 export function hasCurrentView(view: string): boolean {
     return (getCurrentView() === view);
-}
-
-/**
- * Set the current view path.
- */
-export function setCurrentView(view: string) {
-    localStorage.setItem(LS_CURRENT_VIEW_KEY, view)
 }
