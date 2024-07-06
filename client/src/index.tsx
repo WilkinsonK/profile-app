3df7ad6bc30bee436b2b04ca3fe6dfd8ea3d269e
Keenan W. Wilkinson <keenanwilkinson@outlook.com>
2024-07-06 12:48:39 -0500

    replace *.css with *.scss

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import './index.scss';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
